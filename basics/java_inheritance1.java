1246bbdefe43992b37262b678547f74738fa8e31
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-02-14 01:22:44 +0530

    Easy

---
+++ basics/java_inheritance1.java
@@ -0,0 +1,40 @@
+/*
+Output  ->  I am walking
+            I am flying
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+class Animal{
+	void walk()
+	{
+		System.out.println("I am walking");
+	}
+}
+class Bird extends Animal
+{
+	void fly()
+	{
+		System.out.println("I am flying");
+	}
+    void walk()
+    {
+		System.out.println("I am walking");        
+    }
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution{
+   public static void main(String args[]){
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+   }
+}

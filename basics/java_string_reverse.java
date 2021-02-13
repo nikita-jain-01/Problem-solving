7bf094726fb29c0127ad4175d54da2ff1bc5208f
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-02-14 00:34:48 +0530

    Easy

---
+++ basics/java_string_reverse.java
@@ -0,0 +1,25 @@
+/*
+Input   ->  madam
+Output  ->  Yes
+*/
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B="";
+        int i;
+        for(i=A.length()-1;i>=0;i--)
+            B=B+A.charAt(i)+"";
+        if(A.equals(B))
+            System.out.println("Yes");       
+        else
+            System.out.println("No");            
+    }
+}

b1a7653b2a9bd540fbfcc20e0a185f2c3c9c7fb3
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-02-20 17:34:24 +0530

    Print message using class

---
+++ message_class.java
@@ -0,0 +1,11 @@
+class Main
+{
+    void dispMsg(){
+        System.out.println("Code Quotient - Get better at coding");
+      }
+    public static void main(String s[])
+    {
+    Main obj=new Main();
+    obj.dispMsg();
+    }
+}

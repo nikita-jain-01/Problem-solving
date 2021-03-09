d677c692da4435002fcbe13dc479179559ecc4a6
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-09 19:00:21 +0530

    Rotate array in cylindrical order

---
+++ cylindrically_rotate_array.java
@@ -0,0 +1,32 @@
+/*
+Input -> 5
+         1 2 3 4 5
+         2
+
+Output -> 4 5 1 2 3
+*/
+
+
+
+
+import java.util.*;
+public class HelloWorld{
+     public static void main(String []args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int[] ar=new int[n];
+        int i,d,j;
+        for(i=0;i<n;i++)
+            ar[i]=scan.nextInt();
+        d=scan.nextInt();
+        for(i=0;i<d;i++)
+        {
+            int temp=ar[n-1];
+            for(j=n-2;j>=0;j--)
+                ar[j+1]=ar[j];
+            ar[0]=temp;
+        }
+        for(i=0;i<n;i++)
+            System.out.print(ar[i]+" ");
+     }
+}

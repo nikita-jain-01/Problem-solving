70b125dd0735384e9b9fe58c655c30016be0b17a
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-12 23:54:22 +0530

    Search element in a rotated and sorted array

---
+++ search_element_sorted_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class HelloWorld{
+     public static void main(String []args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int[] ar=new int[n];
+        int i,d,j;
+        for(i=0;i<n;i++)
+            ar[i]=scan.nextInt();
+        d=scan.nextInt();
+        for(i=0;i<d;i++)
+        {
+            int temp=ar[n-1];
+            for(j=n-2;j>=0;j--)
+                ar[j+1]=ar[j];
+            ar[0]=temp;
+        }
+        for(i=0;i<n;i++)
+            System.out.print(ar[i]+" ");
+     }
+}

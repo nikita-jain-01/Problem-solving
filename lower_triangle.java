f60a314bf797c8e5fa70c8ff764fce8703822370
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-29 00:29:51 +0530

    To print number a lower triangle

---
+++ lower_triangle.java
@@ -0,0 +1,37 @@
+/*
+Input -> 3
+         1 2 3 4 5 6 7 8 9
+
+Output -> 1 0 0
+          4 5 0
+          7 8 9
+*/
+
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int m=scan.nextInt();
+        int[][] ar=new int[m][m];
+        int i,j;
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<m;j++)
+            ar[i][j]=scan.nextInt();
+        }
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(j<=i)
+                System.out.print(ar[i][j]+" ");
+                else
+                System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
+}

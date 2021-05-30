bc74e03ef53063fe7436f769e939c597588e39e7
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-05-30 23:38:44 +0530

    Current year

---
+++ current_year.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int year;
+        java.util.Calendar c;
+        c = java.util.Calendar.getInstance();
+        year = c.get(c.YEAR);
+        System.out.println("Current Year: " + year);
+        System.out.print("sum: " + 5);
+    }
+}

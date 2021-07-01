8148988df9c42dfb433589d1aa28a27ac28e8fa6
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-01 21:23:18 +0530

    6 is present at first or last index

--- array/first_last_6.java
+++ array/first_last_6.java
@@ -8,11 +8,11 @@ class first_last_6 {
         int[] ar = new int[n];
         for (i = 0; i < n; i++)
             ar[i] = scan.nextInt();
-        int sum = FirstLast6(ar);
-        System.out.println(sum);
+        boolean result = firstLast6(ar);
+        System.out.println(result);
     }
 
-    public static boolean FirstLast6(int[] nums) {
+    public static boolean firstLast6(int[] nums) {
         if (nums[0] == 6 || nums[nums.length - 1] == 6)
             return true;
         else
